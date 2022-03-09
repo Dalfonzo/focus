f55ea9746c1f8c57b13164a4c3f4a1a5dfba09a3
David Pérez <dalfonzo18@gmail.com>
2022-03-09 19:03:22 -0400

    Changes layout styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen text-primary bg-background">
-      <header className="flex justify-end max-w-screen-lg p-8 m-auto text-center align-middle gap-7">
+    <div className="min-h-screen text-text-normal bg-background">
+      <header className="flex justify-end max-w-screen-lg px-4 py-8 m-auto text-center align-middle gap-7">
         <p>
           <Link href="/configuration">Configuration</Link>
         </p>
@@ -12,7 +12,7 @@ const Layout = ({ children }) => {
           <Link href="/signin">Log in</Link>
         </p>
       </header>
-      <main className="max-w-screen-lg m-auto">{children}</main>
+      <main className="max-w-screen-lg px-4 m-auto">{children}</main>
       <footer className="max-w-screen-lg pt-16 m-auto text-center">
         &copy;2022
       </footer>
