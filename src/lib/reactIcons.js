28618279b963539ea0a2f9c8d96ac4d14fc25138
David Pérez <dalfonzo18@gmail.com>
2022-03-21 14:24:28 -0400

    Creates files for libs

---
+++ src/lib/reactIcons.js
@@ -0,0 +1,13 @@
+import { BsNutFill, BsFillPlayFill, BsPause } from 'react-icons/bs'
+import { FaUserAstronaut, FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import { IoIosArrowBack } from 'react-icons/io'
+import { MdRestartAlt } from 'react-icons/md'
+
+export const NutIcon = BsNutFill
+export const ProfileIcon = FaUserAstronaut
+export const SignInIcon = FaSignInAlt
+export const SignOutIcon = FaSignOutAlt
+export const BackArrowIcon = IoIosArrowBack
+export const PlayIcon = BsFillPlayFill
+export const PauseIcon = BsPause
+export const RestartIcon = MdRestartAlt
