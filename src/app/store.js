b92ab3d8cff8e7ad7377514a27d99c6b75bca23e
David Pérez <dalfonzo18@gmail.com>
2022-03-21 14:19:13 -0400

    Adds redux persistor

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,43 @@
 import { configureStore } from '@reduxjs/toolkit'
 import pomodoroReducer from '../features/pomodoro/pomodoroSlice'
 import authReducer from '../features/authentication/authenticationSlice'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+import { persistReducer, createTransform } from 'redux-persist'
+import thunk from 'redux-thunk'
+
+const pomodoroBlacklist = createTransform(
+  null,
+  (state) => {
+    const newState = {
+      ...state,
+      status: 'idle',
+      interval: null,
+      cycleType: 'normal',
+      cycleNumber: 0,
+    }
+    newState.remainingTime = newState.sessionDuration
+    return newState
+  },
+  { whitelist: ['pomodoro'] }
+)
+
+const persistConfig = {
+  timeout: 500,
+  key: 'root',
+  storage,
+  transforms: [pomodoroBlacklist],
+}
+
+const reducers = combineReducers({
+  pomodoro: pomodoroReducer,
+  auth: authReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-  reducer: {
-    pomodoro: pomodoroReducer,
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 })
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import { store } from '../app/store'
 import Sounds from '../components/sounds'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
+  let persistor = persistStore(store)
+
   return (
     <Provider store={store}>
-      <Sounds>
-        <Component {...pageProps} />
-      </Sounds>
+      <PersistGate loading={null} persistor={persistor}>
+        <Sounds>
+          <Component {...pageProps} />
+        </Sounds>
+      </PersistGate>
     </Provider>
   )
 }
